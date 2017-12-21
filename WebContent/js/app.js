acd7bb1f2e5dd5efdd327f1b82c96216c28799be
Unknown <danilokhpp@hotmail.com>
2017-12-21 00:04:23 -0200

    Commit inicial do projeto de site de fotografia
    Projeto Bárbara Alves - Fotografia

---
+++ WebContent/js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-T8UQQeNxk2JO3A6RH5jWbfBVgki6JOw",
+    authDomain: "barbara-alves-fotografia.firebaseapp.com",
+    databaseURL: "https://barbara-alves-fotografia.firebaseio.com",
+    projectId: "barbara-alves-fotografia",
+    storageBucket: "barbara-alves-fotografia.appspot.com",
+    messagingSenderId: "2313726941"
+  };
+  firebase.initializeApp(config);
